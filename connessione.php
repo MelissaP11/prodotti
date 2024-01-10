dc3ce7aec17de770cdcbacdde8e0bc9c98b4e64c
Melissa <melissa.prencisvalle@iismajoranamoncalieri.edu.it>
2024-01-10 11:16:31 +0100

    connessione

--- connessione.php
+++ connessione.php
@@ -7,14 +7,14 @@
     $hostname="localhost";
     $username ="root";
     $password = "";
-    $dbname = "dblogin";
+    $dbname = "prodotti";
     //connessione al server sql con selezione del database
     $conn = mysqli_connect($hostname , $username , $password , $dbname);
     if(!$conn)
         die("<br><br><strong>Errore nella connessione</strong>");
     else
-        print "<br><br><strong>Connessione avvenuta corretamente</strong>";
-    mysqlli_close($conn);
+        print "<br><br><strong>Connessione avvenuta corretamente</strong><br><br>";
+    mysqli_close($conn);
     print "<strong>Ora la connessione è chiusa</strong>";
 ?>
 </body>
